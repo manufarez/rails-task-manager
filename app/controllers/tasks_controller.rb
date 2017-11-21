c1928f2c78b5a95990b9792e6e64ddc86d13e3d1
Manuel Farez <manufarez@gmail.com>
2017-11-21 12:49:47 +0100

    Routes and controller done, only checkbox and CSS styling are missing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,50 @@
 class TasksController < ApplicationController
+  def index         # GET /tasks
+    @tasks = Task.all
+  end
+
+  def show          # GET /tasks/:id
+    @task = Task.find(params[:id])
+  end
+
+  def new           # GET /tasks/new
+    @task = Task.new
+  end
+
+  def create        # POST /tasks
+    @task = Task.new(task_params)
+    @task.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def edit          # GET /tasks/:id/edit
+    @task = Task.find(params[:id])
+  end
+
+  def update        # PATCH /tasks/:id
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    # no need for app/views/tasks/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def destroy       # DELETE /tasks/:id
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/tasks/create.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:title, :details)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   get "restaurants", to: "restaurants#index"
+  get "tasks/", to: "tasks#index"
+  get "task/:id", to: "tasks#show", as: 'task'
+  get "tasks/new", to: "tasks#new"
+  post "tasks/", to: "tasks#create"
+  get "task/:id/edit", to: "tasks#edit", as: 'edit'
+  patch "task/:id", to: "tasks#update"
+  delete "task/:id", to: "tasks#destroy"
 end
